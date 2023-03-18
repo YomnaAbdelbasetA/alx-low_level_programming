331414b0d32470adb4a7f1d493d30bc1982a6a5d
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-17 19:46:59 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -A prgram that prints all possible combinations of single-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 57)
+		{
+			putchar((int)i);
+		}
+		else
+		{
+			putchar((int)i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
