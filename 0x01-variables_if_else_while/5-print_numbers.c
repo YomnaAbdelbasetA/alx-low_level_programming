5fb688698e39fc3bac814675ba018ab07ba8d04e
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-17 19:16:38 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main-A prgram that prints all single digit numbers of base 10 starting from 0
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+	return (0);
+}
