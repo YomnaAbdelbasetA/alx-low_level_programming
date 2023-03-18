a6909bd90fc66038c987ca9010099c204c81b373
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-17 18:46:14 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main-A program that prints all the alphabet execluding q and e
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int lowercase = 97;
+
+	while (lowercase <= 122 && (lowercase != 113 || lowecase != 101))
+	{
+		putchar((char)lowercase);
+		lowercase++;
+	}
+	putchar('\n');
+	return (0);
+}
