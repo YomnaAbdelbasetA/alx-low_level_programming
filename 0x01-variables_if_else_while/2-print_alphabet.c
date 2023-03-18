349b99ee52744d748d276f9b659788220508da41
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-17 18:10:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - a program that prints all the alphabet in lowercase
+ *  Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar((char)i);
+		i++;
+	}
+	putchar('\n');
+}
