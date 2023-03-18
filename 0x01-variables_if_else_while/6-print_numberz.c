805b7c783150ce836d18abbf8f91e9eee475f146
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-17 19:26:36 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main-A prgram that prints all single digit numbers of base 10 starting from 0
+* Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar((int)i);
+	}
+	ptchar('\n');
+	return (0);
+}
