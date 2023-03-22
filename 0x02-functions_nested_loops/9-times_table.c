c61ddb196194c2e8088109dbfcae75727efa4eec
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-21 19:31:54 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,18 +5,28 @@
  */
 void times_table(void)
 {
-	int i, j, res;
+	int i, j, res, x, y;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9 ; j++)
 		{
 			res = i * j;
-			_putchar(res + '0');
+			if (res > 9)
+			{
+				x = res % 10;
+				y = (res / 10);
+				_putchar(y + '0');
+				_putchar(x + '0');
+			}
+			else
+			{
+				_putchar(res + '0');
+			}
 			if (j != 9)
 			{
-			_putchar(',');
-			_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
