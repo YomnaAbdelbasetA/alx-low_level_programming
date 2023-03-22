aa42e153135af8795b3e4d633d39945f8ac4c9b8
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-21 18:19:32 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - a function that prints the sign of a number.
+ * @n: the number to be checked
+ * Return: 1 if number is positive, 0 if it's Zero and -1 if it's negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
