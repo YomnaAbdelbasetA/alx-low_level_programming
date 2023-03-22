97c4255fb0013a8a7bb904096176d0a468ea433d
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-21 19:20:03 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - a function that prints the 9 times table, starting with 0.
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9 ; j++)
+		{
+			res = i * j;
+			_putchar(res + '0');
+			if (j != 9)
+			{
+			_pitchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
