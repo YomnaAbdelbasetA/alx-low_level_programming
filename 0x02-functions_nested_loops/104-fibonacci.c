87aec26e90f9512127be7e24763d8a4d5ac30a40
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-22 17:27:41 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the first 50 Fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long x = 0, y = 1, sum = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = x + y;
+		if (i != 49)
+		{
+			printf("%ld, ", sum);
+		}
+		else
+		{
+			printf("%ld\n", sum);
+		}
+		x = y;
+		y = sum;
+	}
+	return (0);
+}
