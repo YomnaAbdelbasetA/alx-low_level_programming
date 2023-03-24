fa83639242a8e533ac0a40b5a4bc3c679fa49858
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-23 19:34:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two numbers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: the result of multiplication process
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
