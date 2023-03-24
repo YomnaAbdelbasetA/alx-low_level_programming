9e2cfa4d438bacd70089ef279f2b83a5e141a757
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-23 19:41:21 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers -  a function that prints numbers from 0 to 9 except 2, 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 && (i != 2 || i != 4); i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
