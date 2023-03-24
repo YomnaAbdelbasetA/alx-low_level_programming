bea99b656bc22b8f441881ee2d2e5448bdf11979
Yomna Abdelbaset <yomna.abdelbaset2@gmail.com>
2023-03-23 19:42:48 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9 && (i != 2 || i != 4); i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
